18b786067ac3dd52416c2f8c6f5e43a6395beea3
bernicerosita-web <bernicerosita@gmail.com>
2025-09-25 14:56:21 +0530

    first commit

---
+++ task1.js
@@ -0,0 +1,15 @@
+ let x="85";
+let y=Number(x);
+console.log(y);
+ if(y>=90&&y>=100){
+    console.log("Grade: A");
+ }
+else if(y>=75&&y<=89){
+    console.log("Grade: B");
+}
+else if(y>=50&&y<=74){
+    console.log("Grade: C");
+}
+else{
+    console.log("Grade: Fail");
+}
---
+++ task2.js
@@ -0,0 +1,22 @@
+
+let num="12";
+let nums=Number(num);
+if(num%2==0){
+    console.log("even");
+}
+else{
+    console.log("odd");
+}
+let day=4;
+if(day>=1&&day<=5){
+    console.log("It's a weekday");
+}
+else if(day==6||day==7){
+    console.log("It's a weekend");
+}
+else{
+    console.log("Invalid day");
+}
+        
+
+
